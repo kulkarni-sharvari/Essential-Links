ba41c494023ee0787576beefde41fc5c086edb1d
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2024-08-19 14:55:53 +0530

    feat: add constants dir with custome error and status messages

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export { SUCCESS_MESSAGES, ERROR_MESSAGES } from './messages';
+export { HTTP_STATUS_CODE, RESPONSE_STATUS, ERROR_TYPE } from './responses';
+export { BC_DEFAULTS, CONTRACT_NAME, BC_ERROR_CODE, GET_LIST_DEFAULT_PAGE_LIMIT } from './constants';
