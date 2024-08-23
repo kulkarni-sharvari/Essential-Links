a4218f3566739f77317d9cca8a297eff96961d1d
Kulkarni <sharvari.kulkarni@accenture.com>
2024-08-23 14:45:47 +0530

    update and get operations on harvests done

--- src/dtos/teaHarvests.dto.ts
+++ src/dtos/teaHarvests.dto.ts
@@ -1,11 +1,4 @@
-// DTO: Data Transfer Object - used for validating and handling user data in a GraphQL API with the help of decorators.
-
-// These DTOs are used to ensure that the data being passed into your GraphQL API is correctly formatted and validated before being processed further.
-
-// Imports validation decorators from the `class-validator` library
 import { IsNotEmpty } from 'class-validator';
-
-// Imports decorators from `type-graphql`
 import { InputType, Field } from 'type-graphql';
 import { TeaHarvests } from '@/typedefs/teaHarvests.type';
 
@@ -32,4 +25,18 @@ export class TeaHarvestsDto implements Partial<TeaHarvests> {
   @Field()
   @IsNotEmpty()
   location: string;
+}
+@InputType()
+
+// the class will have fields that partially match the `TeaHarvests` type, though they are optional in the context of `Partial<TeaHarvests>`
+export class UpdateTeaHarvestsDto implements Partial<TeaHarvests> {
+
+  @Field()
+  @IsNotEmpty()
+  harvestId: string;
+
+  @Field()
+  @IsNotEmpty()
+  blockchainHash: string;
+
 }
--- src/repositories/harvests.repository.ts
+++ src/repositories/harvests.repository.ts
@@ -1,6 +1,8 @@
-import { TeaHarvestsDto } from "@/dtos/teaHarvests.dto";
+import { TeaHarvestsDto, UpdateTeaHarvestsDto } from "@/dtos/teaHarvests.dto";
 import { TeaHarvestsEntity } from "@/entities/harvests.entity";
 import { DBException } from "@/exceptions/DBException";
+import { User } from "@/interfaces/users.interface";
+import { TeaHarvests } from "@/typedefs/teaHarvests.type";
 
 import { EntityRepository } from "typeorm";
 
@@ -14,7 +16,7 @@ export class TeaHarvestsRepository {
      * @param harvestInput takes harvest input object to create record in db
      * @returns updated row in db
      */
-    async harvestCreate(harvestInput: TeaHarvestsDto): Promise<TeaHarvestsEntity> {
+    async harvestCreate(harvestInput: TeaHarvestsDto): Promise<TeaHarvests> {
         const harvestId = uuidv4();
         try {
             const createHarvestData: TeaHarvestsEntity = await TeaHarvestsEntity.create({
@@ -28,9 +30,37 @@ export class TeaHarvestsRepository {
     }
 
 
-    updateHarvest() { }
+    /**
+     * 
+     * @param harvestInput takes blockchainHash and harvestId as input
+     * @param userData required to get userID
+     * @returns updated DB record
+     */
+    async harvestUpdate(harvestInput: UpdateTeaHarvestsDto, userData: User): Promise<TeaHarvests> {
+        try {
+            // find harvest whose id is {harvestId} created by user {userId}
+            const findHarvest = await TeaHarvestsEntity.findOne({ where: { harvestId: harvestInput.harvestId, userId: userData.id } })
+            if (!findHarvest) throw new DBException(409, "No harvest recorded")
+            else {
+                // perform DB update
+                await TeaHarvestsEntity.update(harvestInput.harvestId, { ...harvestInput })
+                // find updated record
+                const updateHarvest: TeaHarvests = await TeaHarvestsEntity.findOne({ where: { harvestId: harvestInput.harvestId } })
+                return updateHarvest
+            }
+        } catch (error) {
+            throw new DBException(500, error)
+        }
+    }
 
-
-    getHarvest() { }
+    /**
+     * 
+     * @param user gets user from Context
+     * @returns all the harvests done by a user
+     */
+    async readAllHarvestByFarmerId(user: User) : Promise<TeaHarvests[]> { 
+        const allHarvests = await TeaHarvestsEntity.find({where:{userId: user.id}})
+        return allHarvests
+    }
 
 }
